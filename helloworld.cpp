f5666a806cc3c7779077af2bca38cf0f35d034c0
Balazs Domjan <balazs.domjan@pulseon.fi>
2020-01-30 12:37:53 +0100

    Print helloworld n times in C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
- cout << "Hello World!\n"; 
- return 0;
+    if (argc != 2) {
+       std::cerr << "Not enough parameters" << endl;
+       return 1;
+    }
+
+    for (int i = 0; i < stoi(argv[1]); i++) {
+        cout << "Hello World!\n";
+    }
+
+    return 0;
 }
