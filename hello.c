4c7a0eb1669251abf5f1d1dec5de27f09ec63fc5
BenjaminLelle1997 <bendzsi.lelle@gmail.com>
2020-02-03 12:27:00 +0100

    Printf something for n times in C

--- hello.c
+++ hello.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
-int main()
-{
-  printf ("Hello World!\n");
-  return 0;
+#include <stdlib.h>
+#include <errno.h>
+ 
+int main(int argc, char** argv) {
+
+   if (argc < 3) {
+      perror("Not enough parameters! Your quantity or expression is missing!\n");
+      return 1;
+   }
+
+   else if (argc > 3) {
+      perror("You typed too much parameters !\n");
+      return 1;
+   }
+
+   for (int i = 0; i < atoi(argv[1]); ++i) {
+      printf(argv[2]);
+      printf("\n");
+   }
+   return 0;
 }
